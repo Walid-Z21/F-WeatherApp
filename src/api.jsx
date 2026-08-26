aa98e8891008a6e08052113fb293046ad638b115
GrizzlyBeard <zidouh.walid21@gmail.com>
2026-08-26 21:26:33 +0100

    updating api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 export const getWeatherData= async(cityName)=>{
     const response = await fetch (
-        `${import.meta.env.VITE_API_URL}/api/weather?city=${cityName}`
+        `${import.meta.env.VERCEL_API_URL}/api/weather?city=${cityName}`
     )
     return response.json();
 
